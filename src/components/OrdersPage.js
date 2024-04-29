5e13e83409c0963d4d8572dbefe407d632414e06
Aremanda Yasaswini <161938824+AremandaYasaswini@users.noreply.github.com>
2024-04-29 21:11:08 +0530

    Update OrdersPage.js

--- src/components/OrdersPage.js
+++ src/components/OrdersPage.js
@@ -3,10 +3,8 @@ import React from 'react';
 function OrdersPage({ orders, onCancelOrder }) {
 
   const handleCancelOrder = (orderId) => {
-  
     const isConfirmed = window.confirm('Are you sure you want to cancel this order?');
     if (isConfirmed) {
-      
       onCancelOrder(orderId);
     }
   };
@@ -17,14 +15,17 @@ function OrdersPage({ orders, onCancelOrder }) {
       <ul>
         {orders.map((order, index) => (
           <li key={index}>
+            {/* Product image */}
+            <img src={order.product.picture} alt={order.product.name} style={{ width: '100px', height: '100px' }} />
+            {/* Product details */}
             <p>Product Name: {order.product.name}</p>
-            <p>Price: ${order.product.price}</p>
+            <p>Price: {order.product.price}</p>
             <p>Quantity: {order.quantity}</p>
             <p>Size: {order.size}</p>
             <p>Shipping Address:</p>
-            <p>City: {order.shippingAddress.city}</p>
-            <p>State: {order.shippingAddress.state}</p>
             <p>Country: {order.shippingAddress.country}</p>
+            <p>State: {order.shippingAddress.state}</p>
+            <p>City: {order.shippingAddress.city}</p>
             <p>Landmark: {order.shippingAddress.landmark}</p>
             <p>Payment Method: {order.paymentMethod}</p>
             {/* Cancel button */}
